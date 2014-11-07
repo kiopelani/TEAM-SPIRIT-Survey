2ab588fd64d6ec49eb04967ada00af3e99668fec
Ariana Vergara & julian <pair+arianakvergara.juliangallegos@devbootcamp.com>
2014-11-06 16:59:27 -0800

    added users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 require 'faker'
 
 5.times do
-  Survey.create(title: Faker::Company.name)
+  user = User.create(:name => Faker::Name.name, :email => Faker::Internet.email, :password => "spirit")
+  5.times do
+    survey = user.surveys.create(title: Faker::Company.name)
+    10.times do
+      question = survey.questions.create(:content => Faker::Lorem.sentence)
+      10.times do
+        choice = question.choices.create(:choice_content => Faker::Company.bs)
+      end
+    end
+  end
 end
 
-30.times do
-  Question.create(:survey_id => rand(1..5), :content => Faker::Lorem.sentence)
-end
-
-100.times do
-  Choice.create(:question_id => rand(1..30), :choice_content => Faker::Company.bs)
-end
 
